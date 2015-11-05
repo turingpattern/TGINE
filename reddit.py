b8b8606f0e3e6023fb4864c2d234045fdb1c1809
turingpattern <adolfo.sanz@rai.usc.es>
2015-11-05 22:41:55 +0100

    Update reddit.py

--- reddit.py
+++ reddit.py
@@ -1,26 +1,48 @@
-# -*- coding: utf-8 -*-
 import praw
-import os
+import datetime #https://docs.python.org/2/library/datetime.html#datetime-objects
 
-my_user_agent="TGINE:v1.0 by /u /pailroco"
-my_sub_reddit="worldnews"
-limite=5
+
+import xml.etree.ElementTree as ET
+#import os
+
+my_user_agent="windows:www.usc.es:TGINE:v1.0 (by /u /pailroco)"
+my_sub_reddit="nosleep"
+limite=2
 nombre_fichero="000_reddit_test.txt"
-fichero = os.open(nombre_fichero, os.O_CREAT|os.O_RDWR|os.O_APPEND)
+fichero = open(nombre_fichero, 'a')
 
 #def escribe_reddit(contenido):
-#    fichero = os.open('nombrefich.txt','a')
-#    fichero.write(contenido)
+#    fichero = open(nombre_fichero, 'a')
+#    fichero.write(contenido+"\n")
+
+#def lee_xml(arbol):
+    #tree = ET.parse('country_data.xml')
+    #doc_xml = ET.fromstring(submission)  
 
 r = praw.Reddit(user_agent=my_user_agent)
-
+r.config.store_json_result=True #Revisar
 subreddit = r.get_subreddit(my_sub_reddit)
-
-for submission in subreddit.get_hot(limit = limite):
-    print("Title: "+submission.title)
-    print("Text: "+submission.selftext)
-    print("Score: "+"%d" % submission.score)
+#get.last
+for submission_hot in subreddit.get_hot(limit = limite):
+    print("Title: "+submission_hot.title)
+    print("Text: "+submission_hot.selftext)
+    print("Score: "+"%d" % submission_hot.score)
     print("---------------------------------\n")
-    os.write(fichero, submission.title+"\n")
+    fichero.write(submission_hot.title+"\n")
+print(submission_hot.json_dict)
 
-os.close(fichero)
+raiz = ET.Element(submission_hot.title)
+contenido = ET.SubElement(raiz, submission_hot.selftext)
+fecha = ET.SubElement(raiz, datetime.fromtimestamp(submission_hot.created_utc).isoformat())
+#tipo = ET.SubElement(raiz, submission_hot.)
+
+#print(json_dict)
+fichero.close
+
+
+#https://praw.readthedocs.org/en/stable/pages/code_overview.html#praw.objects.Comment.submission
+
+
+#XML
+#https://docs.python.org/3.4/library/xml.etree.elementtree.html#building-xml-documents
+#https://pymotw.com/2/xml/etree/ElementTree/create.html#serializing-xml-to-a-stream
